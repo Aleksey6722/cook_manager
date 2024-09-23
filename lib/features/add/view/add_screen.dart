64322cc53b5d7a25fa22e09ad1a8c68e04f1c2f9
Aleksey <aleksey6722@mail.ru>
2024-09-23 16:28:51 +0500

    initial

--- lib/features/add/view/add_screen.dart
+++ lib/features/add/view/add_screen.dart
@@ -2,11 +2,37 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 
 @RoutePage()
-class AddScreen extends StatelessWidget {
+class AddScreen extends StatefulWidget {
   const AddScreen({super.key});
 
   @override
+  State<AddScreen> createState() => _AddScreenState();
+}
+
+class _AddScreenState extends State<AddScreen> {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            title: Text('Добавить рецепт'),
+            leading: IconButton(onPressed: () {}, icon: Icon(Icons.arrow_back)),
+            backgroundColor: Colors.white,
+            surfaceTintColor: Colors.transparent,
+          ),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: EdgeInsets.only(top: 20, left: 20, right: 20),
+              child: Form(
+                  child: Column(
+                children: [],
+              )),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/features/main/view/main_screen.dart
+++ lib/features/main/view/main_screen.dart
@@ -15,51 +15,56 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Категории'),
-        // backgroundColor: Colors.white,
-      ),
       backgroundColor: Colors.white,
-      body: const SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.only(top: 20, left: 20, right: 20),
-          child: Column(
-            children: [
-              MainMenuContainer(
-                imageUrl: "assets/images/salad.jpg",
-                label: 'Салаты',
-              ),
-              SizedBox(height: 20),
-              MainMenuContainer(
-                imageUrl: "assets/images/snaks.jpg",
-                label: "Закуски",
-              ),
-              SizedBox(height: 20),
-              MainMenuContainer(
-                imageUrl: "assets/images/soup.jpg",
-                label: 'Первые блюда',
-              ),
-              SizedBox(height: 20),
-              MainMenuContainer(
-                imageUrl: "assets/images/main_dishes.jpg",
-                label: 'Главные блюда',
-              ),
-              SizedBox(height: 20),
-              MainMenuContainer(
-                imageUrl: "assets/images/desert.jpg",
-                label: 'Десерты и выпечка',
-              ),
-              SizedBox(height: 20),
-              MainMenuContainer(
-                imageUrl: "assets/images/drinks.jpg",
-                label: 'Напитки',
-              ),
-              SizedBox(height: 20),
-            ],
-          ),
+      body: CustomScrollView(slivers: [
+        SliverAppBar(
+          pinned: true,
+          title: Text('Категории'),
+          backgroundColor: Colors.white,
+          surfaceTintColor: Colors.transparent,
         ),
-      ),
+        SliverToBoxAdapter(
+          child: Padding(
+            padding: EdgeInsets.only(top: 20, left: 20, right: 20),
+            child: Column(
+              children: [
+                MainMenuContainer(
+                  imageUrl: "assets/images/salad.jpg",
+                  label: 'Салаты',
+                ),
+                SizedBox(height: 20),
+                MainMenuContainer(
+                  imageUrl: "assets/images/snaks.jpg",
+                  label: "Закуски",
+                ),
+                SizedBox(height: 20),
+                MainMenuContainer(
+                  imageUrl: "assets/images/soup.jpg",
+                  label: 'Первые блюда',
+                ),
+                SizedBox(height: 20),
+                MainMenuContainer(
+                  imageUrl: "assets/images/main_dishes.jpg",
+                  label: 'Главные блюда',
+                ),
+                SizedBox(height: 20),
+                MainMenuContainer(
+                  imageUrl: "assets/images/desert.jpg",
+                  label: 'Десерты и выпечка',
+                ),
+                SizedBox(height: 20),
+                MainMenuContainer(
+                  imageUrl: "assets/images/drinks.jpg",
+                  label: 'Напитки',
+                ),
+                SizedBox(height: 20),
+              ],
+            ),
+          ),
+        )
+      ]
+          //
+          ),
     );
   }
 }
-
